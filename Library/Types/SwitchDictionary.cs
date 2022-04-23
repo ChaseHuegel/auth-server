1b7d6939ee6bd73d5b98db7e3e933b23d59499b8
Seven <chase@chasehuegel.io>
2022-04-22 20:42:39 -0400

    optimized SwitchDictionary

--- Library/Types/SwitchDictionary.cs
+++ Library/Types/SwitchDictionary.cs
@@ -6,68 +6,66 @@ namespace Swordfish.Library.Types
     {
         private LinkedDictionary<TKey1, TKey2> link = new LinkedDictionary<TKey1, TKey2>();
 
-        private Dictionary<TKey1, TValue> dictionary1 = new Dictionary<TKey1, TValue>();
-
-        private Dictionary<TKey2, TValue> dictionary2 = new Dictionary<TKey2, TValue>();
+        private Dictionary<TKey1, TValue> dictonary = new Dictionary<TKey1, TValue>();
 
         public TValue this[TKey1 key1] {
-            get => dictionary1[key1];
-            set => dictionary1[key1] = value;
+            get => dictonary[key1];
+            set => dictonary[key1] = value;
         }
 
         public TValue this[TKey2 key2] {
-            get => dictionary2[key2];
-            set => dictionary2[key2] = value;
+            get => dictonary[link[key2]];
+            set => dictonary[link[key2]] = value;
         }
 
-        public int Count => dictionary1.Count;
+        public int Count => dictonary.Count;
 
         public void Add(TKey2 key2, TKey1 key1, TValue value) => Add(key1, key2, value);
 
         public void Add(TKey1 key1, TKey2 key2, TValue value)
         {
-            dictionary1.Add(key1, value);
-            dictionary2.Add(key2, value);
+            dictonary.Add(key1, value);
             link.Add(key1, key2);
         }
 
         public void Clear()
         {
-            dictionary1.Clear();
-            dictionary2.Clear();
+            dictonary.Clear();
+            link.Clear();
         }
         
-        public bool Contains(TKey1 key) => link.Contains(key);
+        public bool ContainsKey(TKey1 key1) => dictonary.ContainsKey(key1);
 
-        public bool Contains(TKey2 key) => link.Contains(key);
+        public bool ContainsKey(TKey2 key2) => dictonary.ContainsKey(link[key2]);
 
-        public bool Remove(TKey1 key)
+        public bool ContainsValue(TValue value) => dictonary.ContainsValue(value);
+
+        public bool Remove(TKey1 key1)
         {
-            if (dictionary1.Remove(key))
+            if (link.Remove(key1))
             {
-                dictionary2.Remove(link[key]);
-                link.Remove(key);
+                dictonary.Remove(key1);
                 return true;
             }
 
             return false;
         }
 
-        public bool Remove(TKey2 key)
+        public bool Remove(TKey2 key2)
         {
-            if (dictionary2.Remove(key))
+            TKey1 key1 = link[key2];
+            if (link.Remove(key2))
             {
-                dictionary1.Remove(link[key]);
-                link.Remove(key);
+                dictonary.Remove(key1);
                 return true;
             }
 
             return false;
         }
 
-        public bool TryGetValue(TKey1 key, out TValue value) => dictionary1.TryGetValue(key, out value);
+        public bool TryGetValue(TKey1 key1, out TValue value) => dictonary.TryGetValue(key1, out value);
 
-        public bool TryGetValue(TKey2 key, out TValue value) => dictionary2.TryGetValue(key, out value);
+        public bool TryGetValue(TKey2 key2, out TValue value) => dictonary.TryGetValue(link[key2], out value);
 
     }
 }
